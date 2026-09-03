4ab23e4e4e054d8b69db86d5bdf37c8530e5b3ed
Yoge93 <yogesh.walwalkar@deptagency.com>
2026-09-03 13:34:12 +0530

    in column block addition

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -170,7 +170,7 @@ export default async function decorate(block) {
       });
 
       if (customClassConfig) {
-        const componentRoot = row.querySelector(':scope > [data-aue-component], :scope > [data-aue-model], :scope > .block');
+        const componentRoot = customClassConfig.parentElement.querySelector(':scope > [data-aue-component], :scope > [data-aue-model], :scope > .block');
         const componentName = componentRoot?.getAttribute('data-aue-component')
           || componentRoot?.getAttribute('data-aue-model')
           || Array.from(componentRoot?.classList || []).find((className) => className !== 'block');
